8608ac7b57ba77bd136ffc75d411d8ee258537be
Aaron Lademann <trueson82@gmail.com>
2012-02-08 10:09:38 -0600

    added custom /wp-admin/ section stylesheet to admin-header.php
    WP UPDATE CHANGE - will have to reapply this each time wordpress version is updated

--- domains/portfolio.aaronlademann.com/html/wp-admin/admin-header.php
+++ domains/portfolio.aaronlademann.com/html/wp-admin/admin-header.php
@@ -85,8 +85,12 @@ if ( is_rtl() )
 $admin_body_class .= ' branch-' . str_replace( array( '.', ',' ), '-', floatval( $wp_version ) );
 $admin_body_class .= ' version-' . str_replace( '.', '-', preg_replace( '/^([.0-9]+).*/', '$1', $wp_version ) );
 $admin_body_class .= ' admin-color-' . sanitize_html_class( get_user_option( 'admin_color' ), 'fresh' );
+?>
 
-if ( $is_iphone ) { ?>
+<!-- aaronl: custom styles for admin area (will have to reapply every time wordpress is updated. -->
+<link rel="stylesheet" type="text/css" media="screen" href="/wp-content/resources/custom-admin-styles.css" />
+
+<?php if ( $is_iphone ) { ?>
 <style type="text/css">.row-actions{visibility:visible;}</style>
 <?php } ?>
 </head>
