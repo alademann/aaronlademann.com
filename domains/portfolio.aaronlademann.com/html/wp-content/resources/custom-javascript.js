efc27aa606e074a784e065da6ea442b14f5cfd1a
Aaron Lademann <trueson82@gmail.com>
2012-02-08 22:39:38 -0600

    fixed bug causing the hentries to not have their width set for them

--- domains/portfolio.aaronlademann.com/html/wp-content/resources/custom-javascript.js
+++ domains/portfolio.aaronlademann.com/html/wp-content/resources/custom-javascript.js
@@ -5,9 +5,12 @@ $(document).ready(function() {
 	var hentries = $("#masonry-portfolio").find(".hentry");
 	$.each(hentries, function() {
 		var thumbnail = $(".post-thumb > a > img", this);
-		
+		var pic_width = $(this).width();
+		console.debug(pic_width);
+		$(this).closest(".hentry").css("width", pic_width + "px");
+
 		$(thumbnail).load(function() {
-			var pic_width = $(this).width();
+			pic_width = $(this).width();	
 			//var pic_height = $(this).height();
 			console.debug(pic_width);
 			$(this).closest(".hentry").css("width", pic_width + "px");
