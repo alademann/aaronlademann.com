9c950fceb5a62a06540e757426d988993c0f385d
Aaron Lademann <trueson82@gmail.com>
2012-02-10 14:39:35 -0600

    adding more event / pageview tracking for lightbox / external nav

--- domains/portfolio.aaronlademann.com/html/wp-content/resources/custom-javascript.js
+++ domains/portfolio.aaronlademann.com/html/wp-content/resources/custom-javascript.js
@@ -1,4 +1,113 @@
 ﻿var $ = jQuery.noConflict();
+
+var currPage = window.location.pathname;
+//console.info(currPage);
+
+function caption_as_url(caption) {
+	// strip whitespace
+	var caption_as_url = caption.replace(/ /g,"_");
+	// strip quotes
+	caption_as_url = caption_as_url.replace(/"([^"]*)"/g, "$1");
+	return caption_as_url;
+	//console.info(caption_as_url);
+} // caption_as_url()
+
+function trackLightBoxStuff(pglinks,arr) {
+
+	var lboxTitles = arr;
+
+	//------------------------------ track pagination clicks in google analytics
+
+	$(pglinks).find("li.current > a").addClass("active");
+		var pageLinks = $(pglinks).find("li > a");
+		$.each(pageLinks, function() {
+
+			$(this).live('click', function(e) {
+				if(e.button == 0) {
+
+					if($(this).attr("class") != "active") {
+
+						var pageIndex = $(this).attr("href");
+						pageIndex = parseFloat(pageIndex.substring(pageIndex.lastIndexOf("#") + 1));
+						// this is the caption of the image they selected via the pagination nav
+						var whichPage = lboxTitles[pageIndex];
+
+						//console.info("clicked " + whichPage);
+
+						////////////////////////////// TRACK STUFF NOW ///////////////////////////////////////
+						
+						//console.info(currPage + "?paginate=" + caption_as_url(whichPage));
+
+						_gaq.push(['_trackPageview', currPage + "?paginate=" + caption_as_url(whichPage)]);
+						//_gaq.push(['_trackEvent', 'portfolio', 'likes', folioItemTitle]);
+
+						/////////////////////////////////////////////////////////////////////////////////////
+
+						// remove the one that used to be active
+						$(pageLinks).filter(".active").removeClass("active");
+						// add the one that is now active
+						$(this).addClass("active");
+
+
+					}
+
+				}
+
+			}); // end .live(click)
+
+		});             // END .each(paginationLink)
+
+		//------------------------------ track lightbox views in google analytics
+		var lightboxLink = $(".single #primary").find("a.lightbox");
+		$.each(lightboxLink, function() {
+
+			$(this).bind("click", function() {
+				
+				var imageCaption = $(this).find("> .caption").text();
+				var imageCaption_url = caption_as_url(imageCaption);
+
+				_gaq.push(['_trackEvent', 'portfolio', 'fullsize-view', currPage + "?img=" + imageCaption_url]);
+				
+			}); //end .bind("click")
+
+
+			//TODO: track individual lightbox image pageviews once the lightbox has already opened (forward / backward buttons)
+
+		});   // end .each(lightboxLink)
+
+}
+
+// store all the image titles in a single lightbox in an array
+window.addEventListener('DOMNodeInserted', function(event) {
+	var thisLoaded = event.target.id;
+	//console.info(event.target.id);
+	var lightboxGalleryLink = 'fancybox-right';
+	//console.log(thisLoaded);
+	if(thisLoaded == lightboxGalleryLink) {
+		//console.warn("found the lightbox gallery");
+
+		$('body').animate({
+			top: 0
+		}, 500, function() {
+			// Animation complete.
+			var paginationLinks = $(".single #primary").find(".pagination");
+			var lightboxTitleArray = new Array();
+
+			var i = 0;
+			var lightBoxImage = $(".single #primary").find(".lightbox img");
+			$.each(lightBoxImage, function() {
+				lightboxTitleArray[i] = $(this).attr("alt");
+				//console.info("lightboxTitleArray[" + i + "] = " + lightboxTitleArray[i]);
+				i++;
+			});
+
+			trackLightBoxStuff(paginationLinks, lightboxTitleArray);
+
+		});
+
+	} // END if ($(thisLoaded).is( lightboxGalleryDiv ))
+}, false);     // end addEventListener
+
 $(document).ready(function() {
 
 	// disable right click on all images
@@ -10,7 +119,7 @@ $(document).ready(function() {
 	var hentries = $("#masonry-portfolio").find(".hentry");
 	$.each(hentries, function() {
 
-		// track "likes" in google analytics
+		//------------------------------ track "likes" in google analytics
 		var folioItemTitle = $(this).find(".entry-title").text();
 		//console.info(folioItemTitle);
 		var likeButton = $(this).find(".likeThis").not(".active");
@@ -31,7 +140,7 @@ $(document).ready(function() {
 		}); // end .each(likeButton)
 
 
-		// thumbnail hover effects
+		//------------------------------ thumbnail hover effects
 
 		var thumbnail = $(".post-thumb > a > img", this);
 
@@ -69,5 +178,37 @@ $(document).ready(function() {
 		}
 		);
 
-	});
+	}); // END .each(hentries)
+	
+	//------------------------------ track navigation items that go elsewhere
+		var navBtns = $(".nav").find("a");
+		$.each(navBtns, function() {
+
+			//console.info($(this).text());
+
+			$(this).bind("click", function() {
+
+				var btn_text = $(this).text();
+				switch(btn_text) {
+					case "photos":
+						_gaq.push(['_trackEvent', 'nav_external', 'photos.aaronlademann.com']);
+						break;
+					case "photography":
+						_gaq.push(['_trackEvent', 'nav_external', 'photos.aaronlademann.com']);
+						break;
+					case "contact":
+						_gaq.push(['_trackEvent', 'nav_external', 'contact email']);
+						break;
+					case "resume":
+						_gaq.push(['_trackEvent', 'nav_external', 'linkedin']);
+						break;
+					case "twitter":
+						_gaq.push(['_trackEvent', 'nav_external', 'twitter']);
+						break;
+				}
+
+			}); //end .bind("click");
+
+		}); // END .each(navBtns)
+
 });
