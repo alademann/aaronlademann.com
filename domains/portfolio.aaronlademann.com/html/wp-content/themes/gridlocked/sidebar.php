8016e3d4dda5199dec7bc4b75185db07c50b864c
Aaron Lademann <trueson82@gmail.com>
2012-03-12 21:40:37 -0500

    removed logo from sidebar

--- domains/portfolio.aaronlademann.com/html/wp-content/themes/gridlocked/sidebar.php
+++ domains/portfolio.aaronlademann.com/html/wp-content/themes/gridlocked/sidebar.php
@@ -1,47 +1,5 @@
 <!--BEGIN #sidebar .aside-->
 <aside id="sidebar" class="aside">
-	<!-- BEGIN #logo -->
-	<header id="logo">
-		<?php /*
-		If "plain text logo" is set in theme options then use text
-		if a logo url has been set in theme options then use that
-		if none of the above then use the default logo.png */
-		if (get_option('tz_plain_logo') == 'true') { ?>
-					
-		<?php if(is_folio_home()) { ?>
-		<!-- HOME PAGE -->
-		<h1><?php bloginfo( 'name' ); ?></h1>
-		<?php } ?>
-		<?php if(!is_folio_home()){ ?>
-		<!-- NOT THE HOME PAGE -->
-		<a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a>
-		<?php } ?> 
-
-		<?php } elseif (get_option('tz_logo')) { ?>
-		<?php if(is_folio_home()) { ?>
-		<!-- HOME PAGE -->
-		<h1 style="background-image: url(<?php echo get_option('tz_logo'); ?>);"><?php bloginfo( 'name' ); ?></h1>
-		<?php } ?>
-		<?php if(!is_folio_home()){ ?>
-		<!-- NOT THE HOME PAGE -->
-		<a href="<?php echo home_url(); ?>" rel="nofollow"><img id="logoimg" src="<?php echo get_option('tz_logo'); ?>" alt="<?php bloginfo( 'name' ); ?>"/></a>
-		<?php } ?> 
-		<?php } else { ?>
-		<?php if(is_folio_home()) { ?>
-		<!-- HOME PAGE -->
-		<h1 style="background-image: url(<?php echo get_template_directory_uri(); ?>/images/logo.png);"><?php bloginfo( 'name' ); ?></h1>
-		<?php } ?>
-		<?php if(!is_folio_home()){ ?>
-		<!-- NOT THE HOME PAGE -->
-		<a href="<?php echo home_url(); ?>" rel="nofollow"><img id="logoimg" src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="<?php bloginfo( 'name' ); ?>" /></a>
-		<?php } ?> 
-		<?php } ?>
-    <?php $tagline = get_option('tz_tagline'); ?>
-    <?php if($tagline != '') : ?>
-    <p id="tagline"><?php echo stripslashes($tagline); ?></p>
-    <?php endif; ?>
-	</header>
-  <!-- END #logo -->
 	<?php if(!is_single()) : ?>
 	<?php if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar('Portfolio Sidebar') ) :?> 
   <section class="widget">
