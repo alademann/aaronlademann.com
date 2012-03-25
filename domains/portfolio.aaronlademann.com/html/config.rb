e22f472b423c1887c8f128200e464e15b0f8297f
Aaron Lademann <aaron.lademann@gmail.com>
2012-03-25 17:19:19 -0500

    added an environment option that can be ran when compiling compass (dev/prod compression)

--- domains/portfolio.aaronlademann.com/html/config.rb
+++ domains/portfolio.aaronlademann.com/html/config.rb
@@ -10,13 +10,16 @@ javascripts_dir = "public/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-	output_style = :nested
+# use $ compass compile -e production --force to produce compressed css files for production.
+	output_style = (environment == :production) ? :compressed : :expanded	
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
  relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
+
+
 
 
 # If you prefer the indented syntax, you might want to regenerate this
