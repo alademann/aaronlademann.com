820aa4fbe7b9e28820f1239e7d8309126971fe57
Aaron Lademann <aaron.lademann@gmail.com>
2012-03-31 18:36:01 -0500

    fixed all the jquery scope variable definition problems
    TODO: clean up the way all the footer scripts are defined... its a mess... need to combine logical scripts, get it out of inline php if possible

--- domains/portfolio.aaronlademann.com/html/wp-content/resources/includes/portfolio/footer-scripts.php
+++ domains/portfolio.aaronlademann.com/html/wp-content/resources/includes/portfolio/footer-scripts.php
@@ -4,6 +4,8 @@
 	$isIOS = is_ios();
 ?>
 <script id="nomatterwhat">
+$jqloaded = false;
+
 	Modernizr.load([
 		{
 			load: '//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.js',
@@ -19,8 +21,27 @@
 
 		}]);
 	
-	function load_therest(){
+function load_therest(){
+
+	
 	if(window.jQuery){	
+
+		$jqloaded = true;
+		// GLOBAL VARS
+		var $ = jQuery.noConflict();
+		$jQdir = "/public/js/jquery/";
+		$twBdir = "/public/js/bootstrap/";
+		// -----------
+
+		// CONDITIONAL VARS (use these to ONLY LOAD WHAT YOU NEED)
+		$isBrowserIE = yepnope("h", "ie");
+		$isBrowserIE6_8 = yepnope("h", "lt-ie9");
+		$isIphone = yepnope("h", "iphone");
+		$isIphone = yepnope("h", "ipad");
+		$wp_isPageTypeSingle = yepnope("b", "single");
+		$wp_isPageTypeResume = yepnope("b", "page-template-template-resume-php");
+		$wp_isUserLoggedIn = yepnope("b", "logged-in");
+
 		//head.ready("jquery", function(){		
 			// once jquery is loaded, load all the jquery plugins 'n schtuff
 			
@@ -29,7 +50,15 @@
 				// once masonry is loaded...
 
 				head.js({ infinitescroll: "<?php echo $jQueryScriptDir; ?>infinite-scroll/jquery.infinitescroll.min.js" });
+				head.js({ slidesjs: "<?php echo $jQueryScriptDir; ?>slides.min.jquery.js" });
+				
+				// main template customization
 				head.js({ gridlockedcustom: "<?php echo $jQueryScriptDir; ?>jquery.gridlocked-custom.js"	});
+				
+				head.js({ jqueryhotkeys: "<?php echo $jQueryScriptDir; ?>jquery.hotkeys.js" }, 
+				function(){
+					redirect_CtrlP();
+				});
 		
 			}); // END head.ready("masonry")
 			
@@ -60,8 +89,24 @@
 
 
 
+		function yepnope(el, cl) {
+			var $checkThis;
+			if (el = "b") {
+				$checkThis = $("body");
+			} else if (el = "h") {
+				$checkThis = $("html");
+			} else {
+				// something unexpected happened... don't know what i'm supposed to check.
+				console.warn("ruh roh... yepnope() doesn't recognize " + el + " as a valid parameter. valid params are [h, b]");
+				return false;
+			}
+			// simple function that repeats the same .hasClass() test to set conditional vars in all_set()
+			return $checkThis.hasClass(cl) ? true : false;
+		} // END yepnope()
+
 	} // END if(window.jQuery)
-}
+} // END load_therest()
+
 </script>
 <!--[if (gte IE 6)&(lte IE 8)]>
 	<script id="selectivizr">
@@ -70,28 +115,4 @@
 		}
 	</script>
 <![endif]-->
-<?php } ?>
-<?php if(is_singular()) { ?>
-<script id="singlesonly">
-	head.ready("jquery", function(){
-
-		head.js({ slidesjs: "<?php echo $jQueryScriptDir; ?>slides.min.jquery.js" });
-		
-	}); // END head.ready("jquery")
-</script>
-<?php } ?>
-<?php if( is_page_template('template-resume.php') ){ ?>	
-<script id="resumeScripts"> 
-	
-	head.ready("gridlockedcustom", function(){
-		//var $ = jQuery.noConflict();
-
-		head.js({ jqueryhotkeys: "<?php echo $jQueryScriptDir; ?>jquery.hotkeys.js" }, 
-			function(){
-				redirect_CtrlP();
-			});
-
-	}); // END head.ready(jquery)
-
-</script>
 <?php } ?>
