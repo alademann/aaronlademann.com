523fb84624038e32bb83e725bf6e4211ebf841d3
Aaron Lademann <aaron.lademann@gmail.com>
2012-04-03 16:19:40 -0500

    reducing the number of separate IE stylesheets to speed up compile

--- domains/portfolio.aaronlademann.com/html/wp-content/resources/includes/portfolio/stylesheets.php
+++ domains/portfolio.aaronlademann.com/html/wp-content/resources/includes/portfolio/stylesheets.php
@@ -1,32 +1,29 @@
 <link rel="stylesheet" href="<?php echo public_uri(); ?>/css/print.css" media="print">
-<?php
-if(getBrowser(browser) == 'IE' && getBrowser(majorver) == 6) { ?>
-<!-- Internet Explorer 6 -->
-<link rel="stylesheet" href="<?php echo public_uri(); ?>/css/ie_6.css">
-<?php
-} elseif(getBrowser(browser) == 'IE' && getBrowser(majorver) == 7) { ?>
-<!-- Internet Explorer 7 -->
-<link rel="stylesheet" href="<?php echo public_uri(); ?>/css/ie_7.css">
-<?php
-} elseif(getBrowser(browser) == 'IE' && getBrowser(majorver) == 8) { ?>
-<!-- Internet Explorer 8 -->
-<link rel="stylesheet" href="<?php echo public_uri(); ?>/css/ie_8.css">
-<?php
-} elseif(getBrowser(browser) == 'IE' && getBrowser(majorver) > 8) { ?>
+<?php if(getBrowser(browser) != 'IE'){ ?>
+<link rel="stylesheet" href="<?php echo public_uri(); ?>/css/a_screen.css">
+<?php } 
+	// END IF !IE
+?>
+
+<?php if(getBrowser(browser) == 'IE') 
+{ ?>
+	<?php if(getBrowser(majorver) <= 8) 
+	{ ?>
+<!-- Internet Explorer 6, 7 or 8 -->
+<link rel="stylesheet" href="<?php echo public_uri(); ?>/css/ie_lte8.css">
+	<?php 
+	} // END IF IE6,7,8
+	?>
+	<?php if(getBrowser(majorver) > 8) 
+	{ ?>
 <!-- Internet Explorer 9+ -->
 <link rel="stylesheet" href="<?php echo public_uri(); ?>/css/ie_gt8.css">
-<?php
-} elseif(getBrowser(browser) == 'IE' && getBrowser(majorver) < 6) { ?>
-<!-- Internet Explorer 5 Or Lower (UNSUPPORTED) -->
-<link rel="stylesheet" href="<?php echo public_uri(); ?>/css/ie_6.css">
-<?php
-} elseif(getBrowser(browser) != 'IE') { ?>
-<link rel="stylesheet" href="<?php echo public_uri(); ?>/css/screen.css">				
-<?php
-}?>
-<?php if(getBrowser(browser) == 'IE') { ?>
-<!-- Internet Explorer -->
+	<?php 
+	} // END IF IE9+
+	?>
+<!-- Internet Explorer (global) -->
 <link rel="stylesheet" href="<?php echo public_uri(); ?>/css/ie.css">
-<?php } ?>
-
+<?php 
+} // END IF IE
+?>
 <noscript><link rel="stylesheet" href="<?php echo public_uri(); ?>/css/noscript.css"></noscript>
