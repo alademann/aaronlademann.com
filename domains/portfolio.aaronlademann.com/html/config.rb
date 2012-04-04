3130e6a9c771971487f6cf1d72285f3525432073
Aaron Lademann <aaron.lademann@gmail.com>
2012-04-04 11:47:42 -0500

    using the new rgba_ie function to always return non-rgba values in IE6 and 7
    + also consolidated the location of mixins, functions and vars so that bootstrap works regardless of inclusion in my aa.com setup

--- domains/portfolio.aaronlademann.com/html/config.rb
+++ domains/portfolio.aaronlademann.com/html/config.rb
@@ -29,16 +29,3 @@ javascripts_dir = "public/js"
 # and then run:
 # sass-convert -R --from scss --to sass includes/sass scss && rm -rf sass && mv scss sass
 
-module Sass::Script::Functions
-  def hex(decimal)
-    Sass::Script::String.new("%02x" % decimal)
-  end
-
-	def ie_hex_str(color)
-    assert_type color, :Color
-    alpha = (color.alpha * 255).round.to_s(16).rjust(2, '0')
-		Sass::Script::String.new("##{alpha}#{color.send(:hex_str)[1..-1]}".upcase)	
-	end     
-  declare :ie_hex_str, [:color]
-
-end
